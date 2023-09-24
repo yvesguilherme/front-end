a4ef27d8bdd131ab4afa053a0236048179f7435e
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2023-09-24 17:44:45 -0300

    :bulb:  Adding checking HTTP request parameters

--- angular-testing-course/src/app/courses/services/courses.service.spec.ts
+++ angular-testing-course/src/app/courses/services/courses.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from "@angular/core/testing"
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { CoursesService } from "./courses.service";
-import { COURSES } from "../../../../server/db-data";
+import { COURSES, findLessonsForCourse } from "../../../../server/db-data";
 import { Course } from "../model/course";
 import { HttpErrorResponse } from "@angular/common/http";
 
@@ -104,5 +104,27 @@ describe('CoursesService', () => {
         status: 500,
         statusText: 'Internal Server Error'
       });
+  });
+
+  it('should find a list of lessons', () => {
+    coursesService
+      .findLessons(12)
+      .subscribe(lessons => {
+        expect(lessons).toBeTruthy();
+        expect(lessons.length).toBe(3);
+      });
+    
+    const req = httpTestingController.expectOne(req => req.url === '/api/lessons');
+
+    expect(req.request.method).toEqual('GET');
+    expect(req.request.params.get('courseId')).toEqual('12');
+    expect(req.request.params.get('filter')).toEqual('');
+    expect(req.request.params.get('sortOrder')).toEqual('asc');
+    expect(req.request.params.get('pageNumber')).toEqual('0');
+    expect(req.request.params.get('pageSize')).toEqual('3');
+
+    req.flush({
+      payload: findLessonsForCourse(12).slice(0, 3)
+    });
   });
 });
