0612d4ae31000840e738b6f1f0f32e4504d49606
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2023-09-24 14:26:23 -0300

    :bulb:  Adding Angular HTTP Service Testing

--- angular-testing-course/src/app/courses/services/courses.service.spec.ts
+++ angular-testing-course/src/app/courses/services/courses.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from "@angular/core/testing"
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { CoursesService } from "./courses.service";
+import { COURSES } from "../../../../server/db-data";
 
 describe('CoursesService', () => {
   let coursesService: CoursesService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,9 +17,25 @@ describe('CoursesService', () => {
     });
 
     coursesService = TestBed.inject(CoursesService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should retrieve all courses', () => {
+    coursesService
+      .findAllCourses()
+      .subscribe(courses => {
+        expect(courses).toBeTruthy('No courses returned');
+        expect(courses.length).toBe(12, 'incorrect');
 
+        const course = courses.find(course => course.id == 12);
+
+        expect(course.titles.description).toBe('Angular Testing Course');
+      });
+
+    const req = httpTestingController.expectOne('/api/courses');
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush({payload: Object.values(COURSES)});
   });
 });
