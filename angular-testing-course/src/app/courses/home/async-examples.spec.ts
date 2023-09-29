cb628cc9c1a168673a1fe145a12717307c4e6b58
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2023-09-29 00:38:23 -0300

    :bulb:  Adding understanding the angular fakeAsync testing zone

--- angular-testing-course/src/app/courses/home/async-examples.spec.ts
+++ angular-testing-course/src/app/courses/home/async-examples.spec.ts
@@ -1,11 +1,17 @@
+import { fakeAsync, flush, tick } from "@angular/core/testing";
 import { timer } from "rxjs";
 
 fdescribe('Async Testing Examples', () => {
-  it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
-    let test = false;
 
+  let test: boolean;
+
+  beforeEach(() => {
+    test = false;
+  });
+
+  it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
     setTimeout(() => {
-      console.log('running assertions...');
+      // console.log('running assertions...');
 
       test = true;
 
@@ -16,11 +22,9 @@ fdescribe('Async Testing Examples', () => {
   });
 
   it('Asynchronous test example with Jasmine done() and timer from Rxjs', (done: DoneFn) => {
-    let test = true;
-
     timer(1000)
       .subscribe(() => {
-        
+
         test = false;
 
         expect(test).toBeFalsy();
@@ -28,4 +32,24 @@ fdescribe('Async Testing Examples', () => {
         done();
       });
   });
+
+  it('Asynchronous test example - setTimeout() with tick()', fakeAsync(() => {
+    setTimeout(() => test = true, 1000);
+
+    tick(1000);
+
+    expect(test).toBeTruthy();
+
+  }));
+
+  it('Asynchronous test example - setTimeout() with flush()', fakeAsync(() => {
+    setTimeout(() => test = true, 1000);
+    setTimeout(() => { });
+    setTimeout(() => { }, 5000);
+
+    flush();
+
+    expect(test).toBeTruthy();
+
+  }));
 });
