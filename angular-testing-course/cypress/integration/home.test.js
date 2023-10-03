0563a8df8b27cc57e4b10f700513d912cf543c5f
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2023-10-03 20:17:39 -0300

    :bulb:  Adding simulating user interaction in Cypress e2e tests

--- angular-testing-course/cypress/integration/home.test.js
+++ angular-testing-course/cypress/integration/home.test.js
@@ -1,17 +1,39 @@
 describe('Home Page', () => {
-  it('should display a list of courses.', () => {
+  beforeEach(() => {
     cy.fixture('courses.json').as('coursesJSON');
 
     cy.server();
-    
+
     cy.route('/api/courses', '@coursesJSON').as('courses');
 
     cy.visit('/');
+  });
 
+  it('should display a list of courses.', () => {
     cy.contains('All Courses');
 
     cy.wait('@courses');
 
     cy.get('mat-card').should('have.length', 9);
+  });
+
+  it('should display the advanced courses', () => {
+    cy
+      .get('.mdc-tab')
+      .should('have.length', 2);
+
+    cy
+      .get('.mdc-tab')
+      .last()
+      .click();
+
+    cy
+      .get('.mat-mdc-tab-body-active .mat-mdc-card-title')
+      .its('length')
+      .should('be.gt', 1);
+
+    cy.get('.mat-mdc-tab-body-active .mat-mdc-card-title')
+      .first()
+      .should('contain', 'Angular Security Course');
   });
 });
