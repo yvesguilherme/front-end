576d651c58cddae3a895c23372b5c58bb4ab7caf
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2023-10-24 16:44:42 -0300

    :sparkles:  Configuring an NgRx feature module using NgRx Schematics

--- ngrx-course/src/app/courses/courses.module.ts
+++ ngrx-course/src/app/courses/courses.module.ts
@@ -1,43 +1,39 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {HomeComponent} from './home/home.component';
-import {CoursesCardListComponent} from './courses-card-list/courses-card-list.component';
-import {EditCourseDialogComponent} from './edit-course-dialog/edit-course-dialog.component';
-import {CoursesHttpService} from './services/courses-http.service';
-import {CourseComponent} from './course/course.component';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatSortModule} from '@angular/material/sort';
-import {MatTableModule} from '@angular/material/table';
-import {MatTabsModule} from '@angular/material/tabs';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MatMomentDateModule} from '@angular/material-moment-adapter';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {RouterModule, Routes} from '@angular/router';
-import {compareCourses, Course} from './model/course';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import {compareLessons, Lesson} from './model/lesson';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
+import { HomeComponent } from './home/home.component';
+import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
+import { EditCourseDialogComponent } from './edit-course-dialog/edit-course-dialog.component';
+import { CoursesHttpService } from './services/courses-http.service';
+import { CourseComponent } from './course/course.component';
 
 export const coursesRoutes: Routes = [
   {
     path: '',
     component: HomeComponent
-
   },
   {
     path: ':courseUrl',
     component: CourseComponent
   }
 ];
-
 
 @NgModule({
   imports: [
@@ -75,11 +71,4 @@ export const coursesRoutes: Routes = [
     CoursesHttpService
   ]
 })
-export class CoursesModule {
-
-  constructor() {
-
-  }
-
-
-}
+export class CoursesModule { }
