7dbd4f09a17e647f3bece459afcf4931d6e3254a
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2023-11-29 00:02:19 -0300

    :sparkles:  Adding NgRx feature design - defining actions first in courses module

---
+++ ngrx-course/src/app/courses/course.actions.ts
@@ -0,0 +1,12 @@
+import { createAction, props } from "@ngrx/store";
+
+import { Course } from "./model/course";
+
+export const loadAllCourses = createAction(
+  '[Courses Resolver] Load All Courses'
+);
+
+export const allCoursesLoaded = createAction(
+  '[Load Courses Effect]  All Courses Loaded',
+  props<{courses: Course[]}>()
+);
--- ngrx-course/src/app/courses/model/course.ts
+++ ngrx-course/src/app/courses/model/course.ts
@@ -1,8 +1,8 @@
 
 export interface Course {
   id: number;
-  seqNo:number;
-  url:string;
+  seqNo: number;
+  url: string;
   iconUrl: string;
   courseListIcon: string;
   description: string;
@@ -13,14 +13,14 @@ export interface Course {
 }
 
 
-export function compareCourses(c1:Course, c2: Course) {
+export function compareCourses(c1: Course, c2: Course) {
 
   const compare = c1.seqNo - c2.seqNo;
 
   if (compare > 0) {
     return 1;
   }
-  else if ( compare < 0) {
+  else if (compare < 0) {
     return -1;
   }
   else return 0;
