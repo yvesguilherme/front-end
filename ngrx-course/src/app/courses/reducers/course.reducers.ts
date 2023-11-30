8cee4cd9bb15808c852e7c889ec1f9304f183b6c
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2023-11-29 23:14:00 -0300

    :sparkles:  Adding NgRx data fetching solution - how to load data only if needed

--- ngrx-course/src/app/courses/reducers/course.reducers.ts
+++ ngrx-course/src/app/courses/reducers/course.reducers.ts
@@ -1,14 +1,16 @@
 import { EntityState, createEntityAdapter } from "@ngrx/entity";
-
-import { Course } from "../model/course";
 import { createReducer, on } from "@ngrx/store";
+
+import { Course, compareCourses } from "../model/course";
 import { CourseActions } from "../course-action-types";
 
 export interface CourseState extends EntityState<Course> {
 
 }
 
-export const adapter = createEntityAdapter<Course>();
+export const adapter = createEntityAdapter<Course>({
+  sortComparer: compareCourses
+});
 
 export const inititalCourseState = adapter.getInitialState();
 
