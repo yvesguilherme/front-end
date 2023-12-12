7b5bbc8093f6426b442d53c8676a9dc04f2687d1
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2023-12-11 23:36:43 -0300

    :arrow_up: Upgrade to angular 14

--- ngrx-course/section-6-ngrx-data/src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ ngrx-course/section-6-ngrx-data/src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,7 +1,7 @@
 import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {Course} from '../model/course';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {UntypedFormBuilder, UntypedFormGroup, Validators} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {CoursesHttpService} from '../services/courses-http.service';
 
@@ -12,7 +12,7 @@ import {CoursesHttpService} from '../services/courses-http.service';
 })
 export class EditCourseDialogComponent {
 
-  form: FormGroup;
+  form: UntypedFormGroup;
 
   dialogTitle: string;
 
@@ -23,7 +23,7 @@ export class EditCourseDialogComponent {
   loading$:Observable<boolean>;
 
   constructor(
-    private fb: FormBuilder,
+    private fb: UntypedFormBuilder,
     private dialogRef: MatDialogRef<EditCourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data,
     private coursesService: CoursesHttpService) {
